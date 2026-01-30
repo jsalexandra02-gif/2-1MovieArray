3b591a13107a0fc8aa9a768831e6c2ca35e244e2
Jade Suszek <jsalexandra02@gmail.com>
2026-01-29 19:00:22 -0500

    File Upload

---
+++ 21MovieArray.js
@@ -0,0 +1,14 @@
+let myMovies = ["Matilda", "Snack Shack", "Monsters Inc", "Lisa Frankenstein", "A Simple Favor"];
+//checking the myMovies array
+console.log("These are my favorite movies:", myMovies);
+
+//prompt user for their favorite movie
+let newMovie = window.prompt("What is your favorite movie?");
+//add user input to the myMovies array
+myMovies.push(newMovie);
+
+//checking the addition of the the new movie to the array
+console.log("Updated list of movies:", myMovies);
+
+//changing the array to a string
+console.log("Updated list of movies: ", myMovies.toString());
